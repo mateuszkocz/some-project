a610ec93d4a17523fdcc4e022abce05fae1e4283
Mateusz Kocz <mateusz@kocz.pl>
2019-08-31 14:19:34 +0200

    Add a minor doc

--- src/storyshots.test.ts
+++ src/storyshots.test.ts
@@ -1,3 +1,8 @@
+/**
+ * This file initializes the StoryShots functionality.
+ * Read more: https://github.com/storybookjs/storybook/tree/master/addons/storyshots/storyshots-core
+ */
+
 import initStoryshots from "@storybook/addon-storyshots"
 
 initStoryshots({
