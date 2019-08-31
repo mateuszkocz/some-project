74483af295dbaebc2f3591cd7c1e94b649bee076
Mateusz Kocz <mateusz@kocz.pl>
2019-08-31 09:39:07 +0200

    Add some TS studd

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
     },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
